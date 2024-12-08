603a0ffa67c5314ee14db0a20680e4dcfb0c9962
Sanjeet Kumar <96577772+sanjeetsk@users.noreply.github.com>
2024-12-08 09:19:26 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // write js code here if required
+const para = document.getElementById('timer');
+
+const date = new Date();
+const day = String(date.getDate()).padStart(2,"0");
+const month = String(date.getMonth()+1).padStart(2, "0");
+const year = date.getFullYear();
+const hour = date.getHours();
+const minute = date.getMinutes();
+const second = date.getSeconds();
+
+para.innerHTML = `${day}/${month}/${year}, ${hour}:${minute}:${second}`
